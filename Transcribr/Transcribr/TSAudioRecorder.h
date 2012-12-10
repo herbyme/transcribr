8c67134de3898b5425ae6250d9e11ac8a89bb087
Herbert Yeung <debouncing@gmail.com>
2012-12-10 18:58:40 +1100

    Adding the recording functionality as well as the interfaces we are going to use.

--- Transcribr/Transcribr/TSAudioRecorder.h
+++ Transcribr/Transcribr/TSAudioRecorder.h
@@ -7,7 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Novocaine.h"
+#import "AudioFileWriter.h"
+
+#define AUDIO_MAX_SIZE 144000 // Roughly 30mins of Audio
 
 @interface TSAudioRecorder : NSObject
+{
+    Novocaine *audioManager;
+    AudioFileWriter *fileWriter;
+}
+
+@property (nonatomic, copy) NSString* fileName;
+
+-(BOOL) checkEnoughDiskSpace;
+-(void) startRecording;
+-(void) pauseRecording;
+-(void) stopRecording;
+-(UInt32) audioFileSize;
 
 @end
--- Transcribr/Transcribr/TSAudioRecorder.m
+++ Transcribr/Transcribr/TSAudioRecorder.m
@@ -10,4 +10,31 @@
 
 @implementation TSAudioRecorder
 
+-(void) startRecording {
+    
+    NSArray *pathComponents = [NSArray arrayWithObjects:
+                               [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                               @"MyRecording.m4a",
+                               nil];
+    NSURL *outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+    NSLog(@"URL: %@", outputFileURL);
+    
+    fileWriter = [[AudioFileWriter alloc]
+                  initWithAudioFileURL:outputFileURL
+                  samplingRate:audioManager.samplingRate
+                  numChannels:audioManager.numInputChannels];
+    
+    
+    __block int counter = 0;
+    audioManager.inputBlock = ^(float *data, UInt32 numFrames, UInt32 numChannels) {
+        [fileWriter writeNewAudio:data numFrames:numFrames numChannels:numChannels];
+        counter += 1;
+        if (counter > AUDIO_MAX_SIZE) {
+            audioManager.inputBlock = nil;
+//            [fileWriter release];
+        }
+    };
+
+}
+
 @end
