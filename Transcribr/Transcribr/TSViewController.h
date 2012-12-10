50d3a44ef137e5aa1cf83c329b16d8cfd6e64c8f
Herbert Yeung <debouncing@gmail.com>
2012-12-10 15:17:05 +1100

    Contains XCode Project and subsequent files + groups.

---
+++ Transcribr/Transcribr/TSViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSViewController.h
+//  Transcribr
+//
+//  Created by Herbert Yeung on 10/12/12.
+//  Copyright (c) 2012 becontactable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSViewController : UIViewController
+
+@end
---
+++ Transcribr/Transcribr/TSViewController.m
@@ -0,0 +1,29 @@
+//
+//  TSViewController.m
+//  Transcribr
+//
+//  Created by Herbert Yeung on 10/12/12.
+//  Copyright (c) 2012 becontactable. All rights reserved.
+//
+
+#import "TSViewController.h"
+
+@interface TSViewController ()
+
+@end
+
+@implementation TSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
