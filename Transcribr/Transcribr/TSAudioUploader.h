77d7d2424aa5d9a1ac19b634766db3d4bb276ba0
Herbert Yeung <debouncing@gmail.com>
2012-12-10 18:58:53 +1100

    Same as previous comment.

--- Transcribr/Transcribr/TSAudioUploader.h
+++ Transcribr/Transcribr/TSAudioUploader.h
@@ -8,6 +8,13 @@
 
 #import <Foundation/Foundation.h>
 
+#define CHUNK_SIZE 1000 // 1Mb chunks?!?
+
 @interface TSAudioUploader : NSObject
+
+@property (nonatomic, assign) int chunkIndex;
+
+-(BOOL) checkWifiConnectivity;
+-(BOOL) uploadFile;
 
 @end
