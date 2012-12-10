dd587e6f528bd7f1608134fb2ba05b26fbdc77c7
Herbert Yeung <debouncing@gmail.com>
2012-12-10 19:59:17 +1100

    1. Adding pause and stop recording buttons 2. Adding the property timeleftLabel for countdown.

--- Transcribr/Transcribr/TSViewController.h
+++ Transcribr/Transcribr/TSViewController.h
@@ -8,7 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+#define MAX_TIME 1800 //30 minutes = 1800 seconds
+
 @interface TSViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *TimeLeftLabel;
 
 - (IBAction)recordAction:(id)sender;
 
